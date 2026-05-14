117ed4797629256dc865ed8dd224b628b9c96015
Juan Rodriguez <2305juanda@gmail.com>
2026-05-14 16:30:30 -0500

    feat: I add the lock with object

--- untitled/src/main/java/org/example/classs/BankAccount.java
+++ untitled/src/main/java/org/example/classs/BankAccount.java
@@ -3,6 +3,8 @@ package org.example.classs;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class BankAccount {
+    private final Object balanceLock=new Object();
+    private static final Object totallock=new Object();
     private long balance;
     private static int totalDeposits=0;
     public BankAccount(long balance){
@@ -13,21 +15,18 @@ public class BankAccount {
     }
 
     public  void deposit(long amount) throws InterruptedException {
-        synchronized(this){this.balance+=amount;}
-        Thread.sleep(10);
-        synchronized (BankAccount.class){totalDeposits++;}
+        synchronized(balanceLock){this.balance+=amount;}
+        synchronized (totallock){totalDeposits++;}
    }
     public void withdraw(long amount) throws InterruptedException {
-        synchronized(this){this.balance-=amount;}
-        Thread.sleep(10);
+        synchronized(balanceLock){this.balance-=amount;}
     }
 
     public void setBalance(long balance) throws InterruptedException {
-        synchronized(this){this.balance=balance;}
-        Thread.sleep(10);
+        synchronized(balanceLock){this.balance=balance;}
     }
 
     public long getBalance() throws InterruptedException {
-        synchronized(this){return this.balance;}
+        synchronized(balanceLock){return this.balance;}
     }
 }
