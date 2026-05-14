b58ab6f1616b0046b9e284a590b726f22f643889
Juan Rodriguez <2305juanda@gmail.com>
2026-05-13 22:19:17 -0500

    feat: add synchronized with this and static

--- untitled/src/main/java/org/example/classs/BankAccount.java
+++ untitled/src/main/java/org/example/classs/BankAccount.java
@@ -3,26 +3,31 @@ package org.example.classs;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class BankAccount {
-    private AtomicLong balance;
-    public BankAccount(AtomicLong balance){
+    private long balance;
+    private static int totalDeposits=0;
+    public BankAccount(long balance){
         this.balance = balance;
     }
     public BankAccount() {
-        this.balance=new AtomicLong(0);
+        this.balance=0;
     }
 
-    public void deposit(long amount){
-        this.balance.addAndGet(amount);
-    }
-    public void withdraw(long amount){
-        this.balance.addAndGet(-amount);
+    public  void deposit(long amount) throws InterruptedException {
+        synchronized(this){this.balance+=amount;}
+        Thread.sleep(10);
+        synchronized (BankAccount.class){totalDeposits++;}
+   }
+    public void withdraw(long amount) throws InterruptedException {
+        synchronized(this){this.balance-=amount;}
+        Thread.sleep(10);
     }
 
-    public void setBalance(long balance) {
-        this.balance.set(balance);
+    public void setBalance(long balance) throws InterruptedException {
+        synchronized(this){this.balance=balance;}
+        Thread.sleep(10);
     }
 
-    public long getBalance() {
-        return this.balance.get();
+    public long getBalance() throws InterruptedException {
+        synchronized(this){return this.balance;}
     }
 }
